d5f925aa2a0a5ed486aa22cf3b2f02afc866f6b1
Katie Allyn <kaallyn@gmail.com>
2018-05-14 22:40:15 -0500

    homework 1, first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+var tech = ["Uber", "Lyft", "Google", "Apple", "Shazzam"];
+var animals = ["kittens", "puppers", "baby birds", "mexican walking fish", "lemurs"];
+var text = "";
+var i;
+
+for (i = 0; i < animals.length; i++) {
+  console.log("My company is the " tech[i] " for " animals[i]);
+};
